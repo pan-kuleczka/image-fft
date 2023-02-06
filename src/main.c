918a38115111473994bd649d594c0ba6383bef1a
pan-kuleczka <80748387+pan-kuleczka@users.noreply.github.com>
2023-02-06 22:28:53 +0100

    Added interface and updated Makefile prerequisites

--- src/main.c
+++ src/main.c
@@ -4,11 +4,37 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Hello, world!\n");
+    int degA, degB;
+    int *polyA, *polyB;
 
-    int polyA[] = {1, 2, 3};
-    int polyB[] = {1, 2, 3};
-    int *polyC = multiplyPolynomials(3, polyA, 3, polyB);
-    printf("%d\n", polyC[2]);
+    printf(u8"Wpisz stopień pierwszego wielomianu: ");
+    scanf("%d", &degA);
+    ++degA;
+    polyA = malloc(sizeof(int) * degA);
+    printf(u8"Wpisz współczynniki pierwszego wielomianu: ");
+    for (int i = degA - 1; i >= 0; --i)
+        scanf("%d", polyA + i);
+
+    printf(u8"Wpisz stopień drugiego wielomianu: ");
+    scanf("%d", &degB);
+    ++degB;
+    polyB = malloc(sizeof(int) * degB);
+    printf(u8"Wpisz współczynniki drugiego wielomianu: ");
+    for (int i = degB - 1; i >= 0; --i)
+        scanf("%d", polyB + i);
+
+    int *polyC = multiplyPolynomials(degA, polyA, degB, polyB);
+    printf(u8"Wynik to:\n");
+    for (int i = (degA + degB - 1) - 1; i >= 0; --i)
+    {
+        printf("%dx**%d", polyC[i], i);
+        if (i != 0)
+            printf(" + ");
+    }
+    printf("\n");
+
+    free(polyA);
+    free(polyB);
+    free(polyC);
     return 0;
 }
